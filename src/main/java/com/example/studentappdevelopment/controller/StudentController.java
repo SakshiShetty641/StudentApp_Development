5ea945e2442f1094076eb25e47f0f5a648b9e259
Sakshi <sakshishetty1231997@gmail.com>
2021-10-10 13:19:29 +0530

    Added: Students to the list in the database

--- src/main/java/com/example/studentappdevelopment/controller/StudentController.java
+++ src/main/java/com/example/studentappdevelopment/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.example.studentappdevelopment.entity.Student;
 import com.example.studentappdevelopment.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +20,10 @@ public class StudentController {
     @GetMapping(value = "/students")
     public List<Student> students() {
         return studentService.students();
+    }
+
+    @PostMapping(value = "/student")
+    public Student addStudent(@RequestBody Student students) {
+        return studentService.addStudent(students);
     }
 }
--- src/main/java/com/example/studentappdevelopment/service/StudentService.java
+++ src/main/java/com/example/studentappdevelopment/service/StudentService.java
@@ -3,9 +3,11 @@ package com.example.studentappdevelopment.service;
 import com.example.studentappdevelopment.entity.Student;
 import com.example.studentappdevelopment.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class StudentService {
 
     @Autowired
@@ -13,6 +15,10 @@ public class StudentService {
 
     public List<Student> students() {
         return studentRepository.findAll();
+    }
+
+    public Student addStudent(Student students) {
+        return studentRepository.save(students);
     }
 }
 
