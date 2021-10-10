697a6b0aa7001bdeaec317b1621a8187815b1cb7
Sakshi <sakshishetty1231997@gmail.com>
2021-10-10 11:58:37 +0530

    Added: The dependencies required for student app development

---
+++ src/main/java/com/example/studentappdevelopment/StudentAppDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentappdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAppDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAppDevelopmentApplication.class, args);
+	}
+
+}
